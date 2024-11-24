8f791837042c7a5a249340d152ecb9b18c43d36b
Bruno Henrique <brunohmfcorporativo@gmail.com>
2024-11-24 15:08:02 -0300

    Feat: Implement multiple errors validations.
    TODO: Reformat and restruture project

--- Controllers/BaseController.cs
+++ Controllers/BaseController.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+using Geocoding;
 using HumbertoMVC.Controllers.systemControllers;
 using HumbertoMVC.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -14,36 +16,98 @@ public class BaseController : Controller
     private readonly AdressController _ConvertAndress;
     private readonly RouteController _routeController;
     private readonly GeoCodingService _geoCoding;
+    private readonly IHttpContextAccessor _httpContextAccessor;
 
-    public BaseController(AdressController convertAndress, RouteController routeController, GeoCodingService geoCoding)
+    public BaseController(AdressController convertAndress, RouteController routeController, GeoCodingService geoCoding, IHttpContextAccessor  httpContextAccessor)
     {
         _routeController = routeController ;
         _ConvertAndress = convertAndress ;
         _geoCoding = geoCoding;
+        _httpContextAccessor = httpContextAccessor;
+    }
+
+    [HttpGet]
+    [Route("redirected")]
+    public async Task<IActionResult> Redirected(bool redirected)
+    {
+       
+        Console.Error.WriteLine($"Ocorreu um erro ao exibir as rotas verifique as sessoes de rota \n" +
+                                $"Metodo ExibirRotas \n" +
+                                $"Status Code: {404} \n" + 
+                                "Redirecionado para a Home Page");
+        return StatusCode(404, new
+        {
+            
+            userMessage = "Ops! tivemos um problema, tente buscar a rota novamente",
+            devMessage = "Ocorreu um erro ao buscar as rotas da session RotasOnibus, \n verifique os logs"
+        });
+        
     }
     
     [HttpPost]
     [Route("iniciarBusca")]
     public async Task<IActionResult> IniciarBusca([FromBody] LocalizacaoEnderecoRquest request)
     {
-            
-            // RECEBER ENDERECO FROM FUNCTION BUSCAR
-            string destinyFromBody = request.endereco;
-            Adress.GeoResponse enderecoConvert = await _geoCoding.ConvertAdress(destinyFromBody);
-            string enderecoConvertString = JsonConvert.SerializeObject(enderecoConvert); 
-            
-            TempData["adressDestiny"] = enderecoConvertString;
-            
-            if (enderecoConvertString == null || enderecoConvertString == "")
+        // Valida coordenadas origem
+        string? sessionCord = _httpContextAccessor.HttpContext?.Session.GetString("CoordenadasOrigem") ?? string.Empty;
+        LocationModel.Coordenadas origemCord =
+            JsonConvert.DeserializeObject<LocationModel.Coordenadas>(sessionCord);
+        
+        if (string.IsNullOrEmpty(sessionCord))
+        {
+            Console.Error.WriteLine($"Erro ao buscar na session os dados da cordenada: {sessionCord} \n" +
+                                    $"Session acessada: CoordenadasOrigem \n" +
+                                    $"Erro ocorrido no metodo Armazenar Rotas \n");
+            return StatusCode(422, new
             {
-                Console.WriteLine("Endereço de destino está vazio, verifique o processo de converção! ");
-                return BadRequest("Não foi possivel encontrar o destino informado");
-            }   
+                userMessage = "Opa! tivemos um problema com sua localização, Verifique se o GPS está ligado e tente novamente.",
+                devMessage = "Erro ao buscar na session os dados da cordenada verifique o log sessionCord: " + sessionCord + " \n"
+            });
+        }
+        else if (origemCord.Latitude == null || origemCord.Longitude == null)
+        {
+            Console.Error.WriteLine($"Erro ao buscar cordenadas de origem, Latitude e Longitude.  \n" +
+                                    $"Latitude Origem: {origemCord.Latitude} \n" +
+                                    $"Longitude Origem: {origemCord.Longitude} \n" +
+                                    $"Erro ocorrido no metodo Armazenar Rotas \n");
+            return StatusCode(422, new
+            {
+                userMessage = "Opa! tivemos um problema com sua localização, Verifique se o GPS está ligado.",
+                devMessage = "Faltam cordanadas de origem verifique o log para mais detalhes."
+            });
+        }
+        
+        // Recebe endereço destino da funçãoo Buscar.
+        string destinyFromBody = request.endereco;
+        Adress.GeoResponse destinyConverted = await _geoCoding.ConvertAdress(destinyFromBody); // Latitude e Longitude do destino
+        
+        // Valida retorno conversão Destino
+        if (destinyConverted.Status == "ZERO_RESULTS")
+        {
+            Console.Error.WriteLine("Endereço de destino está vazio, verifique o processo de converção!");
+            return StatusCode(404, new
+            {
+                userMessage = "Ops! O endereço digitado não foi encontrado!", 
+                devMessage = "Endereço de destino está vazio, verifique o processo de converção!"
+            });
+        }
+        else if (destinyConverted.Status != "OK" )
+        {
+            Console.WriteLine("Convert Adress StatusCode: " + destinyConverted.Status);
+            return StatusCode(500, new
+            {
+                userMessage = "Ops! Ocorreu um erro em nosso sistema. \n Tente novamente mais tarde",
+                devMessage = "Convert Adress StatusCode: " + destinyConverted.Status
+            });
+        }
             
-            Console.WriteLine("*** Destino armazenado com sucesso ***");
+            
             LocationModel.Coordenadas destCoord = new LocationModel.Coordenadas();
-            //Armazenar a rota
-            foreach (var result in enderecoConvert.Results)
+            /*  cria o objeto destCoord com os dados retornados da conversão.
+                navegando pela lista: result.Geometry.Location, pegando a ultima localização.
+                armazenando em: destCoord.Coordenadas.
+             */
+            foreach (var result in destinyConverted.Results)
             {
                 double latitude = result.Geometry.Location.Lat;
                 double longitude = result.Geometry.Location.Lng;
@@ -53,35 +117,46 @@ public class BaseController : Controller
                     Longitude = longitude,
                 };
             };
-               
-                IActionResult ArmazenarRotaResult = await _routeController.ArmazenarRotaFinal(destCoord);
-                string rotaArmazenadaString = JsonConvert.SerializeObject(ArmazenarRotaResult);
-                var statusCode = (ArmazenarRotaResult as ObjectResult)?.StatusCode 
-                                 ?? (ArmazenarRotaResult as StatusCodeResult)?.StatusCode;
-                if (statusCode == 200)
-                {
-                    Console.WriteLine("Dados de rota armazenados com sucesso - Rota armazenada: " + rotaArmazenadaString);   
-                }
-                else
-                {
-                    Console.WriteLine("\n \n Ocorreu um erro ao calcular sua rota StatusCode: " + statusCode + "\n Exception: " + (ArmazenarRotaResult as ObjectResult)?.Value);
-                    return BadRequest("Falha ao calcular sua rota!");
-                }
-                
-                
+
+            // Armazena as rotas;
+            IActionResult ArmazenarRotaResult = await _routeController.ArmazenarRotas(destCoord, origemCord);
             
-            Console.WriteLine("Rota calculada e armazenada com sucesso");
+            //Validar erros
+            var statusCode = (ArmazenarRotaResult as ObjectResult)?.StatusCode 
+                             ?? (ArmazenarRotaResult as StatusCodeResult)?.StatusCode;
+            if (statusCode != 200)
+            {
+                var objectResult = (ArmazenarRotaResult as ObjectResult)?.Value?.ToString();
+                var errorText = JsonConvert.DeserializeObject<errorLogModel>(objectResult);
+                
+                Console.Error.WriteLine($"Erro ao executar ArmazenarRotas: \n" +
+                                        $"StatusCode: {statusCode} \n" +
+                                        $"Erro ocorrido no metodo ArmazenarRotas. \n");
+                return StatusCode(500, new
+                {
+                    userMessage = errorText.userMessage,
+                    devMessage = errorText.devMessage
+                });
+            }
+            
             //Exibir rotas
             try
             {
-            _routeController.ExibirRotas();
-            Console.WriteLine("Rota Exibir executado");
-            return Ok();
+                _routeController.ExibirRotas();
+           
+            return Ok(new { devMessage = "Rotas armazenadas com sucesso!" });
             }
             catch (Exception e)
             {
-              Console.WriteLine(e);
-               return BadRequest("Não foi possivel exibir sua rota!");
+                Console.Error.WriteLine($"Ocorreu um erro interno no metodo ExibirRotas \n" +
+                                        $"Status Code: {500} \n" +
+                                        $"Exception: {e} \n");
+                return StatusCode(500, new
+                {
+                    userMessage = "Ocorreu um erro interno, tente novamente mais tarde.",
+                    devMessage = $"Ocorreu um erro interno, verifique os logs,  \n" +
+                                 $"Exception: {e}  \n"
+                });
             }
         
     }
